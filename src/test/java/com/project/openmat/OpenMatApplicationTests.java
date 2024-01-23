13a285dbc7b25208df55da299901c0f2f6a47d18
chansoo <chansoo38@gmail.com>
2024-01-23 10:20:43 +0900

    INIT : 프로젝트 환경 설정

---
+++ src/test/java/com/project/openmat/OpenMatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.openmat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenMatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
